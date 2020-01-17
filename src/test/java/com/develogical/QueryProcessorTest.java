fb391a4801f57d05b98b7e52f27f4290e55ad6ac
Matthew Robinson <matt-robinson@hotmail.com>
2020-01-17 13:20:52 +0000

    Fixed test that was failing...

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -12,7 +12,7 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is(""));
+        assertThat(queryProcessor.process("test1"), is(""));
     }
 
     @Test
