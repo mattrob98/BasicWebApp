131a5139d17bde7126622e337e102383201db8ef
Michael Tsang <michaeltsang1998@gmail.com>
2020-01-21 11:24:14 +0000

    two funcs

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -16,6 +16,23 @@ public class QueryProcessor {
         } else if (query.toLowerCase().contains("team name")) {
             return "Benedrome Cucaracha";
         }
-        return "";
+        return process2(query);
+    }
+
+    public String process2(String query) {
+        if (query.toLowerCase().contains("shakespeare")) {
+            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("cucumber")) {
+            return "benzo";
+        } else if (query.toLowerCase().contains("benzo")) {
+            return "is a drug, whoops";
+        } else if (query.toLowerCase().contains("test1")) {
+            return "This is a test";
+        } else if (query.toLowerCase().contains("team name")) {
+            return "Benedrome Cucaracha";
+        }
+        return ""
     }
 }
