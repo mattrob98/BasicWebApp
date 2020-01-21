96fa590d43a26ccf5f80705f4a53b433efd11e1a
Matthew Robinson <matt-robinson@hotmail.com>
2020-01-21 11:51:48 +0000

    Theresa May

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -30,8 +30,8 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("cucumber")) {
-            return "benzo";
+        } else if (query.toLowerCase().contains("theresa may")) {
+            return "2016";
         } else if (query.toLowerCase().contains("benzo")) {
             return "is a drug, whoops";
         } else if (query.toLowerCase().contains("test1")) {
